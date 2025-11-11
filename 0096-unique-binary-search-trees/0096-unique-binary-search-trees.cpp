bfbcfca8f6bb8a884937ce239a6cfe0d7a80821a
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-11-11 16:42:49 +0700

    Time: 0 ms (100%), Space: 8 MB (38.1%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -5,7 +5,6 @@ public:
 
         for (int i = 2; i <= n; i++) {
             int total = 0;
-            cout << i << endl;
             for (int j = 1; j <= i; j++) {
                 int left = j - 1; 
                 int right = i - j;
