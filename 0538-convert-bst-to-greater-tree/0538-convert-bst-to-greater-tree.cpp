6683d5122d12c7421d60f1725060e513f9ed3aaa
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2025-11-19 17:50:30 +0700

    Time: 0 ms (100%), Space: 35 MB (82.08%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
@@ -10,24 +10,33 @@
  * };
  */
 class Solution {
-public:
-
-    int temp = 0;
-
-    void dfs(TreeNode* root){
-        if(root){
-            dfs(root->right);
-            int res = root->val + temp;
-            temp += root->val;
-            root->val = res;
-
-            dfs(root->left);   
-                 
+private:
+    int calculate(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
         }
+
+        int leftResult = calculate(root -> left);
+        int rightResult = calculate(root -> right);
+
+        return leftResult + rightResult + root -> val;
     }
 
+    void inorder(TreeNode* root, int &sum) {
+        if (root == NULL) {
+            return;
+        }
+
+        inorder(root -> left, sum);
+        int prevRootValue = root -> val;
+        root -> val = sum;
+        sum -= prevRootValue;
+        inorder(root -> right, sum);
+    }
+public:
     TreeNode* convertBST(TreeNode* root) {
-        dfs(root);
+        int sum = calculate(root);
+        inorder(root, sum);
 
         return root;
     }
