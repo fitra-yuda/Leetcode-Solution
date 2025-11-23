f52ad33c5321111892b5e83d93eda2c31380146b
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2025-11-23 21:03:10 +0700

    Time: 19 ms (9.19%), Space: 11 MB (27.71%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,106 +1,106 @@
-
-struct Node{
-    int positionX, positionY;
-    
-    Node(int x, int y) : positionX(x), positionY(y){}
-};
-
 class Solution {
+private:
+
+    bool check(vector<pair<int,int>> &positions, int row, int col, int n) {
+        bool result = true;
+        // check row
+        for (int currentRow = 0; currentRow < n; currentRow++) {
+            if (currentRow == row) {
+                continue;
+            }
+
+            for (auto [r, c] : positions) {
+                if (currentRow == r && c == col) {
+                    result = false;
+                }
+            }
+        }
+
+        // move to top left
+        for (int currentRow = row - 1, currentCol = col - 1; currentRow >= 0 && currentCol >= 0; currentRow--, currentCol--) {
+            
+            for (auto [r , c] : positions) {
+                if (currentRow == r && currentCol == c) {
+                    result = false;
+                }
+            }
+        }
+
+        // move to bottom right
+        for (int currentRow = row + 1, currentCol = col + 1; currentRow < n && currentCol < n; currentRow++, currentCol++) {
+            
+            for (auto [r , c] : positions) {
+                if (currentRow == r && currentCol == c) {
+                    result = false;
+                }
+            }
+        }
+
+        // move to top right
+        for (int currentRow = row - 1, currentCol = col + 1; currentRow >= 0 && currentCol < n; currentRow--, currentCol++) {
+            
+            for (auto [r , c] : positions) {
+                if (currentRow == r && currentCol == c) {
+                    result = false;
+                }
+            }
+        }
+
+        // move to bottom left
+        for (int currentRow = row + 1, currentCol = col - 1; currentRow < n && currentCol >= 0; currentRow++, currentCol--) {
+            
+            for (auto [r , c] : positions) {
+                if (currentRow == r && currentCol == c) {
+                    result = false;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    void construct(vector<vector<pair<int,int>>> &results, vector<pair<int,int>> &positions, int row, int n, int total) {
+        if (total == n) {
+            results.push_back(positions);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (check(positions, row, col, n) == false) {
+                continue;
+            }
+
+            positions.push_back({row, col});
+            construct(results, positions, row + 1, n, total + 1);
+            positions.pop_back();
+        }
+    }
 public:
-    
-    bool checkValidTile(vector<Node> queenPosition, Node currentPosition, int n){
-        
-        bool check = true;
-        
-        // check column
-        for(auto data : queenPosition){
-            if(data.positionY == currentPosition.positionY){
-                check = false;
-            }
-        }
-        
-        // check diagnoal
-        for(auto data : queenPosition){
-            int queenPositionX = data.positionX;
-            int queenPositionY = data.positionY;
-            
-            // left bottom diagnoal of the current queen position
-            while(queenPositionX < n && queenPositionY >= 0){
-                if(currentPosition.positionX == queenPositionX && currentPosition.positionY == queenPositionY){
-                    check = false;
-                }
-                
-                queenPositionX++;
-                queenPositionY--;
-            }
-            // right bottom diagnoal of the  current queen position
-            queenPositionX = data.positionX;
-            queenPositionY = data.positionY;
-            while(queenPositionX < n && queenPositionY < n){
-                if(currentPosition.positionX == queenPositionX && currentPosition.positionY == queenPositionY){
-                    check = false;
-                }
-                
-                queenPositionX++;
-                queenPositionY++;
-            }
-        }
-        
-        return check;
-    }
-    
-     void pushAnswer(vector<vector<string>> &answers, vector<Node> queenPosition, int n){
-        
-        vector<string> tempAnswers;
-        for(int i = 0; i < n; i++){
-            string temp = "";
-            for(int j = 0; j < n; j++){
-                temp += '.';
-            }
-            
-            tempAnswers.push_back(temp);
-        }
-        
-        answers.push_back(tempAnswers);
-        
-        for(auto data : queenPosition){
-            //cout << data.positionX << " " << data.positionY << endl;
-            answers[answers.size() - 1][data.positionX][data.positionY] = 'Q';
-        }
-    }
-    
-    void recursive(int currentRow, int n, vector<Node> queenPosition, vector<vector<string>> &answers){
-        
-        for(int column = 0; column < n; column++){
-            Node currentPosition(currentRow, column);
-           
-            if(checkValidTile(queenPosition, currentPosition, n) == true){
-                queenPosition.push_back(currentPosition);
-                //cout << currentPosition.positionX << " " << currentPosition.positionY << endl;
-                
-                if(currentRow == n - 1){
-                    pushAnswer(answers, queenPosition, n);
-                }
-                
-                recursive(currentRow + 1, n , queenPosition, answers);
-                queenPosition.erase(queenPosition.begin() + queenPosition.size() - 1);
-            }
-        }
-    }
-    
-   
-    
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> answers;
-        vector<Node> queenPosition;
-        
-        recursive(0, n, queenPosition, answers);
-        
-        return answers;
+        vector<vector<pair<int,int>>> results;
+        vector<pair<int, int>> positions;
+
+        construct(results, positions, 0, n, 0);
+
+        vector<vector<string>> answer;
+
+        for (auto positions : results) {
+            vector<string> temp;
+            for (int row = 0; row < n; row++) {
+                string map = "";
+                for (int col = 0; col < n; col++) {
+                    map += '.';
+                }
+                temp.push_back(map);
+            }
+
+            for (auto [row, col] : positions) {
+                temp[row][col] = 'Q';
+            }
+
+            answer.push_back(temp);
+        }
+
+        return answer;
     }
 };
-
-
-
-
-
