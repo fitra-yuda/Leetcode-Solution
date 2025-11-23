e288179a289b0e806f21bbc7da672d74c09abba1
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2025-11-23 21:13:29 +0700

    Time: 0 ms (100%), Space: 10.1 MB (92.24%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,106 +1,53 @@
 class Solution {
 private:
-
-    bool check(vector<pair<int,int>> &positions, int row, int col, int n) {
-        bool result = true;
-        // check row
-        for (int currentRow = 0; currentRow < n; currentRow++) {
-            if (currentRow == row) {
-                continue;
-            }
-
-            for (auto [r, c] : positions) {
-                if (currentRow == r && c == col) {
-                    result = false;
-                }
-            }
-        }
-
-        // move to top left
-        for (int currentRow = row - 1, currentCol = col - 1; currentRow >= 0 && currentCol >= 0; currentRow--, currentCol--) {
-            
-            for (auto [r , c] : positions) {
-                if (currentRow == r && currentCol == c) {
-                    result = false;
-                }
-            }
-        }
-
-        // move to bottom right
-        for (int currentRow = row + 1, currentCol = col + 1; currentRow < n && currentCol < n; currentRow++, currentCol++) {
-            
-            for (auto [r , c] : positions) {
-                if (currentRow == r && currentCol == c) {
-                    result = false;
-                }
-            }
-        }
-
-        // move to top right
-        for (int currentRow = row - 1, currentCol = col + 1; currentRow >= 0 && currentCol < n; currentRow--, currentCol++) {
-            
-            for (auto [r , c] : positions) {
-                if (currentRow == r && currentCol == c) {
-                    result = false;
-                }
-            }
-        }
-
-        // move to bottom left
-        for (int currentRow = row + 1, currentCol = col - 1; currentRow < n && currentCol >= 0; currentRow++, currentCol--) {
-            
-            for (auto [r , c] : positions) {
-                if (currentRow == r && currentCol == c) {
-                    result = false;
-                }
-            }
-        }
-
-        return result;
-    }
-
-    void construct(vector<vector<pair<int,int>>> &results, vector<pair<int,int>> &positions, int row, int n, int total) {
-        if (total == n) {
-            results.push_back(positions);
+    void construct(vector<vector<string>> &results, vector<string> &board, int row, int n) {
+        if (row >= n) {
+            results.push_back(board);
             return;
         }
 
         for (int col = 0; col < n; col++) {
-            if (check(positions, row, col, n) == false) {
+            if (isSafe(board, row, col, n) == false) {
                 continue;
-            }
+            } 
 
-            positions.push_back({row, col});
-            construct(results, positions, row + 1, n, total + 1);
-            positions.pop_back();
+            board[row][col] = 'Q';
+            construct(results, board, row + 1, n);
+            board[row][col] = '.';
         }
+    }
+
+    bool isSafe(vector<string> &board, int row, int col, int n) {
+        // check row
+        for (int currentRow = 0; currentRow < row; currentRow++) {
+            if (board[currentRow][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // check top left
+        for (int currentRow = row - 1, currentCol = col - 1; currentRow >= 0 && currentCol >= 0; currentRow--, currentCol--) {
+            if (board[currentRow][currentCol] == 'Q') {
+                return false;
+            }
+        }
+
+        // check top right
+        for (int currentRow = row - 1, currentCol = col + 1; currentRow >= 0 && currentCol < n; currentRow--, currentCol++) {
+            if (board[currentRow][currentCol] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
     }
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<pair<int,int>>> results;
-        vector<pair<int, int>> positions;
+        vector<vector<string>> results;
+        vector<string> board(n, string(n, '.'));
 
-        construct(results, positions, 0, n, 0);
+        construct(results, board, 0,  n);
 
-        vector<vector<string>> answer;
-
-        for (auto positions : results) {
-            vector<string> temp;
-            for (int row = 0; row < n; row++) {
-                string map = "";
-                for (int col = 0; col < n; col++) {
-                    map += '.';
-                }
-                temp.push_back(map);
-            }
-
-            for (auto [row, col] : positions) {
-                temp[row][col] = 'Q';
-            }
-
-            answer.push_back(temp);
-        }
-
-        return answer;
+        return results;
     }
 };
