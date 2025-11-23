445f9680b4142403c7e19e706b20378b873b9ab4
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2025-11-23 07:33:25 +0700

    Time: 0 ms (100%), Space: 9.2 MB (37.58%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,23 +1,23 @@
 class Solution {
-public:
-    
-    void dfs(vector<int>& nums, int currentIndex, int &answer, int currentXor){
-        if(currentIndex == nums.size()) return;
-        
-        answer += currentXor;
-        
-        for(int i = currentIndex + 1; i < nums.size(); i++){
-            dfs(nums, i, answer, currentXor ^ nums[i]);
+private:
+    int calculate(vector<int> &nums, int index, int currentResult) {
+
+        if (index >= nums.size()) {
+            return 0;
         }
+
+        int sum = 0;
+        while (index < nums.size()) {
+            sum += currentResult ^ nums[index];
+            int result = calculate(nums, index + 1, currentResult ^ nums[index]);
+            sum += result;
+            index++;
+        }
+
+        return sum;
     }
-    
+public:
     int subsetXORSum(vector<int>& nums) {
-        
-        int answer = 0;
-        for(int i = 0; i < nums.size(); i++){
-            dfs(nums, i, answer, nums[i]);
-        }
-        
-        return answer;
+       return calculate(nums, 0, 0); 
     }
 };
