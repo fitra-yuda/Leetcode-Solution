c79510d33b67a77d7010a1422f5db699bf5472b6
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2025-11-24 08:57:58 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,42 +1,50 @@
 class Solution {
-public:
-    
-    bool dfs(vector<vector<char>>& board, string word, int row, int column, int index){
+private:
+    bool check(vector<vector<char>> &board, string &word, string &current, int row, int col) {
+        if (current.length() == word.length()) {
+            return current == word;
+        } else if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size()) {
+            return false;
+        } else if (current.length() > 0 && current[current.length() - 1] != word[current.length() - 1]) {
+            return false;
+        } else if (board[row][col] == '.') {
+            return false;
+        } 
+
+        current += board[row][col];
+        board[row][col] = '.';
+        cout << current << endl;
+        // move to the top
+        bool top = check(board, word, current, row - 1, col);
+
+        // move to the bottom
+        bool bottom = check(board, word, current, row + 1, col);
         
-        if(index == word.length()){
-            return true;
-        }
+        // move to the right
+        bool right = check(board, word, current, row, col + 1);
         
-        if(row < 0 || column < 0) return false;
-        else if(row >= board.size() || column >= board[0].size()) return false;
-        else if(board[row][column] != word[index]) return false;
-        
-        // cout << row << " " << column << " " << board[row][column] << " " << word[index] << endl;
-        
-        board[row][column] = '0';
-        int y[] = {0,1,0,-1};
-        int x[] = {1,0,-1,0};
-        
-        bool result = false;
-        for(int i = 0; i < 4; i++){
-            result = dfs(board, word, row + y[i], column + x[i], index + 1);
-            if(result) break;
-        }
-        
-        board[row][column] = word[index];
-        
-        return result;
+        // move to the left
+        bool left = check(board, word, current, row, col - 1);
+
+        board[row][col] = current[current.length() - 1];
+        current.pop_back();
+
+        return top || bottom || right || left;
     }
-    
+public:
     bool exist(vector<vector<char>>& board, string word) {
         
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[i].size(); j++){
-                if(dfs(board, word, i, j , 0)) return true;
+        for (int row = 0; row < board.size(); row++) {
+            for (int col = 0; col < board[0].size(); col++) {
+                string current = "";
+                bool result = check(board, word, current, row, col);
+
+                if (result) {
+                    return true;
+                }
             }
         }
-        
+
         return false;
-        
     }
 };
