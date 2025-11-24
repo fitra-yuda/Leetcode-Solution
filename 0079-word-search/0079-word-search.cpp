a54b93162dd65cfd15cb670a84083c0b229bf26a
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2025-11-24 09:01:36 +0700

    Time: 231 ms (67.97%), Space: 10.6 MB (71.23%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,33 +1,30 @@
 class Solution {
 private:
-    bool check(vector<vector<char>> &board, string &word, string &current, int row, int col) {
-        if (current.length() == word.length()) {
-            return current == word;
+    bool check(vector<vector<char>> &board, string &word, int row, int col, int index) {
+        if (index >= word.size()) {
+            return true;
         } else if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size()) {
             return false;
-        } else if (current.length() > 0 && current[current.length() - 1] != word[current.length() - 1]) {
+        } else if (board[row][col] != word[index]) {
             return false;
         } else if (board[row][col] == '.') {
             return false;
         } 
 
-        current += board[row][col];
         board[row][col] = '.';
-        cout << current << endl;
         // move to the top
-        bool top = check(board, word, current, row - 1, col);
+        bool top = check(board, word, row - 1, col, index + 1);
 
         // move to the bottom
-        bool bottom = check(board, word, current, row + 1, col);
+        bool bottom = check(board, word, row + 1, col, index + 1);
         
         // move to the right
-        bool right = check(board, word, current, row, col + 1);
+        bool right = check(board, word, row, col + 1, index + 1);
         
         // move to the left
-        bool left = check(board, word, current, row, col - 1);
+        bool left = check(board, word, row, col - 1, index + 1);
 
-        board[row][col] = current[current.length() - 1];
-        current.pop_back();
+        board[row][col] = word[index];
 
         return top || bottom || right || left;
     }
@@ -36,8 +33,7 @@ public:
         
         for (int row = 0; row < board.size(); row++) {
             for (int col = 0; col < board[0].size(); col++) {
-                string current = "";
-                bool result = check(board, word, current, row, col);
+                bool result = check(board, word, row, col, 0);
 
                 if (result) {
                     return true;
