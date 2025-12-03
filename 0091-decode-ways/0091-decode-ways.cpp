04a1b132ca498c9bae8488e88e27fb7b71951a68
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2025-12-03 14:46:51 +0700

    Time: 0 ms (100%), Space: 9.3 MB (24.55%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,25 +1,46 @@
+/*
+
+DP State : 
+dp[i][j]
+- j == 0 -> dp[i + 1][j + 1] + dp[i + 1][j + 2]
+- j == 1 -> dp[i + 2][j + 1] + dp[i + 2][j + 2]
+
+- if the current number == 0 || current number > 26, then return 0
+
+*/
+
 class Solution {
 public:
     int numDecodings(string s) {
-        
-        vector<int> dp(s.length() + 1, 0);
-        
-        dp[0] = 1;
-        dp[1] = s[0] == '0' ? 0 : 1;
-            
-        for(int i = 2; i < dp.size(); i++){
-            
-            if(s[i - 1] != '0'){
-                dp[i] = dp[i - 1];
+        vector<vector<int>> dp(s.length(), vector<int>(2, 0));
+
+        // basecase
+        if (s[s.length() - 1] != '0') {
+            dp[s.length() - 1][0] = 1;
+        }
+
+        for (int i = s.length() - 2; i >= 0; i--) {
+
+            if (s[i] == '0') {
+                dp[i][0] = 0;
+                dp[i][1] = 0;
+            } else {
+                // take only one digit
+                dp[i][0] = dp[i + 1][0] + dp[i + 1][1];
+
+                // take two digit
+                if (i + 1 < s.length() && s[i] >= '1' && s[i] <= '2') {
+                    if (s[i] == '2' && s[i + 1] > '6') {
+                        dp[i][1] = 0;
+                    } else if (i + 2 >= s.length()) {
+                        dp[i][1] = 1;
+                    } else {
+                        dp[i][1] = dp[i + 2][0] + dp[i + 2][1];
+                    }
+                }
             }
-            
-            string currentNumber = s.substr(i - 2, 2);
-            if(currentNumber >= "10" && currentNumber <= "26"){
-                dp[i] += dp[i - 2];
-            }
-            
         }
         
-        return dp[s.length()];
+        return dp[0][0] + dp[0][1];
     }
 };
