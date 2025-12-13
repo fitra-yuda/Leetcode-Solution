5393c71793fbdf65893d3b5ba9006a7d491aa438
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2025-12-13 08:06:40 +0700

    Time: 7 ms (27.43%), Space: 30.8 MB (28.05%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,37 +1,53 @@
 class Solution {
-public:
-    
-    int answer = 0;
-    
-    void dfs(vector<vector<int>>& grid, int row, int column, int &current){
-        if(row < 0 || column < 0 || row >= grid.size() || column >= grid[0].size()){
-            return;
+private:
+    int bfs(vector<vector<int>> &grid, int row, int col) {
+
+        int moveRow[] = {-1,0,1,0}; 
+        int moveCol[] = {0,1,0,-1};
+
+        queue<pair<int,int>> q;
+        q.push({row, col});
+        grid[row][col] = 0;
+
+        int result = 1;
+
+        while (q.size() > 0) {
+            auto [row, col] = q.front();
+            q.pop();
+
+            
+            for (int i = 0; i < 4; i++) {
+                int nextRow = moveRow[i] + row;
+                int nextCol = moveCol[i] + col;
+
+                if (nextRow < 0 || nextRow >= grid.size() || nextCol < 0 || nextCol >= grid[0].size()) {
+                    continue;
+                } else if (grid[nextRow][nextCol] == 0) {
+                    continue;
+                }
+                q.push({nextRow, nextCol});
+                grid[nextRow][nextCol] = 0;
+
+                result++;
+            }
         }
-        
-        if(grid[row][column] == 0) return;
-        
-        grid[row][column] = 0;
-        
-        current++;
-        dfs(grid, row - 1, column, current);
-        dfs(grid, row + 1, column, current);
-        dfs(grid, row, column - 1, current);
-        dfs(grid, row, column + 1, current);
-        
+
+        return result;
     }
-    
+public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[i].size(); j++){
-                int current = 0;
-                if(grid[i][j] == 1){
-                    dfs(grid, i, j, current);
-                    answer = max(current, answer);
+        int result = 0;
+        int totalRow = grid.size();
+        int totalCol = grid[0].size();
+
+        for (int row = 0; row < totalRow; row++) {
+            for (int col = 0; col < totalCol; col++) {
+                if (grid[row][col] == 1) {
+                    result = max(bfs(grid, row, col), result);
                 }
             }
         }
-        
-        return answer;
+
+        return result;
     }
 };
