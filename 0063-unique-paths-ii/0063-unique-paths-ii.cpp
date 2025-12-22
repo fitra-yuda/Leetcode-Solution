9b34fe79c6f46a5a74ded471fe7be3b271841430
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2025-12-22 16:44:15 +0700

    Time: 0 ms (100%), Space: 11.5 MB (87.91%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -7,39 +7,67 @@ DP State :
 DP[i][j] = represent the total ways to reach end of the grid start from i-th row and j-th col
 
 DP Transtition : 
-DP[i][j] = dp[i + 1][j] + dp[i][j + 1]
 
+TOP DOWN
+DP[i][j] = dp[i + 1][j] + dp[i][j + 1]
 - dp[i + 1][j] = move to the next row or bottom
 - dp[i][j + 1] = move to the next col or right
+
+BOTTOM UP
+DP[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+DP Optimize Space : 
+- This only works for bottom up approach
+- We don't need an additional space for every single cell that we have, we just need to save previous row
+
+dp[i] = dp[j - 1] + dp[j]
+
+Final State : 
+
+BOTTOM UP
+- return cell (0,0)
+
+TOP DOWN
+- return dp[i][j] | dp[totalCol - 1]
 
 */
 
 class Solution {
-private:
-    int dfs(int i, int j, vector<vector<int>> &obstacleGrid, vector<vector<int>> &dp) {
-       if (i == obstacleGrid.size() - 1 && j == obstacleGrid[0].size() - 1) {
-            return 1;
-       } else if (i >= obstacleGrid.size() || j >= obstacleGrid[0].size()) {
-            return 0;
-       } else if (obstacleGrid[i][j] == 1) {
-            return 0;
-       }
-
-       if (dp[i][j] != -1) {
-            return dp[i][j];
-       }
-
-       // move to the bottom
-       int bottomResult = dfs(i + 1, j, obstacleGrid, dp);
-       // move to the right
-       int rightResult = dfs(i, j + 1, obstacleGrid, dp);
-
-        dp[i][j] = bottomResult + rightResult;
-        return dp[i][j];
-    }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        vector<vector<int>> dp(obstacleGrid.size(), vector<int>(obstacleGrid[0].size(), -1));   
-        return dfs(0, 0, obstacleGrid, dp);
+
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        int totalRow = obstacleGrid.size();
+        int totalCol = obstacleGrid[0].size();
+
+        vector<int> dp(totalCol, 0);
+        for (int i = 0; i < totalRow; i++) {
+            for (int j = 0; j < totalCol; j++) {
+                if (i == 0 && j == 0) {
+                    dp[j] = 1;
+                } else if (i == 0) {
+                    if (obstacleGrid[i][j] == 1) {
+                        dp[j] = 0;
+                    } else {
+                        dp[j] = dp[j - 1];
+                    }
+                } else if (j == 0) {
+                    if (obstacleGrid[i][j] == 1) {
+                        dp[j] = 0;
+                    }
+                } else {
+                    if (obstacleGrid[i][j] == 1) {
+                        dp[j] = 0;
+                    } else {
+                        dp[j] = dp[j - 1] + dp[j];
+                    }
+                }
+            }
+        }
+
+        return dp[totalCol - 1];
     }
 };
