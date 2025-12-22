ceaadfcb5b0864dcad7b3e5124dc9cacfcfa63f6
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2025-12-22 21:02:37 +0700

    Time: 0 ms (100%), Space: 12.5 MB (85.24%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+
+        if (triangle.size() == 1) {
+            return triangle[0][0];
+        }
+
+        int result = INT_MAX;
+        for (int i = 1; i < triangle.size(); i++) {
+            for (int j = 0; j < triangle[i].size(); j++) {
+                if (j == 0) {
+                    triangle[i][j] += triangle[i - 1][j];
+                } else if (j == triangle[i].size() - 1) {
+                    triangle[i][j] += triangle[i - 1][j - 1];
+                } else {
+                    triangle[i][j] += min(triangle[i - 1][j - 1], triangle[i - 1][j]);
+                }
+
+                if (i == triangle.size() - 1) {
+                    result = min(result, triangle[i][j]);
+                }
+            }
+        }
+
+        return result;
+    }
+};
