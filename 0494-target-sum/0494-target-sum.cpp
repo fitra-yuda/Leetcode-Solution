cc38a4c7d8c9ea081ce79543b13d68c0dd50a77c
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2025-12-28 14:50:10 +0700

    Time: 170 ms (32.94%), Space: 60.2 MB (9.93%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,26 +1,33 @@
 class Solution {
-public:
-    
-    int backtracking(vector<int>& nums, vector<unordered_map<int,int>>& dp, int index, int sum, int target){
-        if(index == nums.size()) {
-            if(target == sum) return 1;
-            else return 0;
+private:
+    int dfs(vector<int> &nums, unordered_map<int,unordered_map<int,int>> &dp, int index, int sum, int target) {
+        if (index >= nums.size()) {
+            if (target == sum) {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        if (dp.count(index) > 0 && dp[index].count(sum) > 0) {
+            return dp[index][sum];
         }
         
-        if(dp[index].count(sum)) return dp[index][sum];
-        
-        int positive = backtracking(nums, dp, index + 1, sum + nums[index], target);
-        int negative = backtracking(nums, dp, index + 1, sum - nums[index], target);
-        
-        dp[index][sum] = positive + negative;
-        
-        return dp[index][sum];
+        // choose positif sign
+        int result = dfs(nums, dp, index + 1, sum + nums[index], target);
+        // choose negative sign 
+        result += dfs(nums, dp, index + 1, sum - nums[index], target);
+
+        return dp[index][sum] = result;
     }
-    
+public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        
-        vector<unordered_map<int,int>> dp(nums.size() + 1);
-        
-        return backtracking(nums , dp, 0, 0, target);;
+        int sum = 0;
+
+        for (auto num : nums) sum += num;
+
+        unordered_map<int, unordered_map<int,int>> dp;
+
+        return dfs(nums, dp, 0, 0, target);
     }
 };
