d9fbd4920d18f3ea4a8e422baa27999e2fd42556
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2025-12-30 06:38:02 +0700

    Time: 3 ms (91.81%), Space: 13 MB (73.84%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,30 +1,38 @@
 class Solution {
-public:
-    
-    int checkSame(char x , char y){
-        if(x == y) return 0;
-        else return 1;
-    }
-    
-    int minDistance(string word1, string word2) {
-        int sizeWord1 = word1.length();
-        int sizeWord2 = word2.length();
-        vector<vector<int>> dp(sizeWord1 + 1, vector<int>(sizeWord2 + 1, 0));
-        
-        for(int i = 1; i <= sizeWord1; i++){
-            dp[i][0] = i;
-        }        
-        
-        for(int i = 1; i <= sizeWord2; i++){
-            dp[0][i] = i;
+private:
+    int dfs(string &s1, string &s2, vector<vector<int>> &dp, int i, int j) {
+        if (i == s1.length() && j == s2.length()) {
+            return 0;
+        } else if (i >= s1.length()) {
+            return s2.length() - j;
+        } else if (j >= s2.length()) {
+            return s1.length() - i;
         }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int result = INT_MAX;
+        if (s1[i] == s2[j]) {
+            result = dfs(s1, s2, dp, i + 1, j + 1);
+        } else {
+            // choose to insert 
+            result = dfs(s1, s2, dp, i, j + 1) + 1;
+
+            // choose to delete
+            result = min(result, dfs(s1, s2, dp, i + 1, j) + 1);
+
+            // choose to replace
+            result = min(result, dfs(s1, s2, dp, i + 1, j + 1) + 1);
+        }
+
+        return dp[i][j] = result;
+    }
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.length(), vector<int>(word2.length(), -1));
         
-        for(int i = 1; i <= sizeWord1; i++){
-            for(int j = 1; j <= sizeWord2; j++){
-                if(word1[i - 1] != word2[j - 1])  dp[i][j] = min(dp[i - 1][j], min(dp[i - 1][j - 1], dp[i][j - 1])) + 1;
-                else dp[i][j] = dp[i - 1][j - 1];
-            }
-        }   
-        return dp[sizeWord1][sizeWord2];
+        return dfs(word1, word2, dp, 0, 0); 
     }
 };
