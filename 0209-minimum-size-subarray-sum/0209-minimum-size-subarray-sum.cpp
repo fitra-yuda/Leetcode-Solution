de0340b09ab6c38d9cbce1bdc6c1f59c42373174
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2026-02-09 11:00:42 +0700

    Time: 0 ms (100%), Space: 32.2 MB (40.08%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,50 +1,25 @@
-/*
-
-How to solve the problem
-
-# Using sliding window
-1. Initialize variables
-   - left = 0
-   - right = 0
-   - result = INT_MAX
-   - total = 0
-2. while right < nums.size()
-   - total += nums[right]
-   - while (total >= target)
-     - result = min(result, (right - left) + 1)
-     - total -= nums[left]
-     - increase left pointer by 1
-   - increase right pointer by 1 
-
-Time Complexity : O(N)
-N -> size of nums
-
-Memory Complexity : O(1)
-
-*/
-
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int left = 0;
         int right = 0;
+        int sum = 0;
         int result = INT_MAX;
-        int total = 0;
 
         while (right < nums.size()) {
-            total += nums[right];
+            sum += nums[right];
 
-            while (total >= target) {
+            while (sum >= target) {
                 result = min(result, (right - left) + 1);
-                total -= nums[left];
+                sum -= nums[left];
                 left++;
             }
 
             right++;
         }
-
+        
         if (result == INT_MAX) {
-            return 0;
+            result = 0;
         }
 
         return result;
