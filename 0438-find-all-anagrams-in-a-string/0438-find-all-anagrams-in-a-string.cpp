212a098b4eb16452202edfa01f6cda705f8e8e1f
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2026-02-10 22:28:35 +0700

    Time: 3 ms (78.51%), Space: 11.7 MB (36.33%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,71 +1,44 @@
-/*
-
-How to solve the problem : 
-1. Construct hashtable with key = lower english letter, and value = total character that appear in hash table
-2. Loop from first index of P until last index of P
-   - Put the character into the hashtable hashTable[p[i]]++
-3. Loop from first index of s until last index of s
-   - Create the sub array with size P
-   - put the left pointer in the most left index of sub array
-   - put the right pointer in the most right index of sub array
-   - Remove current character from hashtable -> hashtable[s[right]]--
-   - if the current sub array length == P, then loop for every character in hashtable and check if all the value in hashtable data == 0, then mark current left value as an answer
-     - Remove most right character to the hashtable -> hashtable[s[left]]--
-     - Add most left character to the hasthable -> hashtable[s[right]]++
-     - Move left pointer to left pointer + 1
-   - Move right pointer to right pointer + 1
-
-Time Complexity : O((N - M) * K)
-N -> size of string s
-M -> size of string p
-K -> size of hashtable -> max(26)
-
-Memory Complexity : O(K + N)
-K -> size of hashtable -> max(26)
-N -> size of string s
-
-*/
-
 class Solution {
+private:
+    bool isValidIndex(vector<int> &bucket) {
+        bool result = true;
+        
+        for (int i = 0; i < bucket.size(); i++) {
+            if (bucket[i] > 0) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
 public:
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<char,int> hashTable;        
-        vector<int> answer;
+        vector<int> results;
+        vector<int> bucket(26, 0);
+        
+        for (auto character : p) {
+            bucket[character - 'a']++;
+        }
+
+        int windowSize = p.length();
         int left = 0;
         int right = 0;
 
-        for (auto c : p) {
-            hashTable[c]++;
-        }
+        while (right < s.length()) {
+            bucket[s[right] - 'a']--;
 
-        while(right < s.length()) {
-            
-            hashTable[s[right]]--;
-
-            if ((right - left) + 1 >= p.length()) {
-                bool valid = true;
-
-                for (auto data : hashTable) {
-                    char key = data.first;
-                    int value = data.second;
-
-                    if (value != 0) {
-                        valid = false;
-                        break;
-                    }
+            if ((right - left) + 1 == windowSize) {
+                if (isValidIndex(bucket)) {
+                    results.push_back(left);
                 }
-
-                if (valid) {
-                    answer.push_back(left);
-                }
-
-                hashTable[s[left]]++;
+                bucket[s[left] - 'a']++;
                 left++;
             }
 
             right++;
-        } 
+        }
 
-        return answer;
+        return results;
     }
 };
