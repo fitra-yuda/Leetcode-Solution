0a89531ef1b942c99ced47e3e71d962c8318b555
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2026-02-10 08:35:54 +0700

    Time: 0 ms (100%), Space: 13 MB (83.29%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,60 +1,31 @@
-/*
-
-How to solve the problem
-
-# Using recursive
-1. void recursive functions (int n, string current, open bracket, close bracket, result)
-   - if current.length() == n * 2
-     - if current is valid answer, then push current to result
-   - else if open bracket > n
-     - return
-   - else if close brakcet > open bracket
-     - return
-   - choose open bracket
-     - recursive (n, current + '(', open bracket + 1, close bracket + 1)
-   - choose close bracket
-     - recursive (n, current + '(', open bracket + 1, close bracket + 1)
-2. initialize variables
-   - vector<string> results
-3. call recrusive(n, "", 0, 0, results)
-4. return results
-
-Time Complexity : O(2^(N * 2) * (N * 2)) = O(2^N)
-N -> pairs of parentheses
-
-Memory Complexity : O(2^N * N) = O(2^N)
-N -> pairs of parentheses
-
-*/
-
 class Solution {
 private:
-    void recursive(int n, string current, int openBracket, int closeBracket, vector<string> &results) {
-        if (current.length() == n * 2) {
-            results.push_back(current);
+    void dfs(vector<string> &results, string &result, int openBracket, int closeBracket, int n) {
+
+        if (result.length() == n * 2) {
+            results.push_back(result);
             return;
         }
 
-        if (openBracket < n) {
-            // choose open bracket
-            current += "(";
-            recursive(n, current, openBracket + 1, closeBracket, results);
-            current.pop_back();
-        } 
-        
-        if (closeBracket < openBracket) {
-            // choose close bracket
-            current += ")";
-            recursive(n, current, openBracket, closeBracket + 1, results);
-            current.pop_back();
+        if (openBracket >= closeBracket) {
+
+            if (openBracket < n) {
+                result += '(';
+                dfs(results, result, openBracket + 1, closeBracket, n);
+                result.pop_back();
+            }
+
+            result += ')';
+            dfs(results, result, openBracket, closeBracket + 1, n);
+            result.pop_back();
         }
     }
-
 public:
     vector<string> generateParenthesis(int n) {
-        vector<string> results;        
+        vector<string> results;
+        string result = "";
 
-        recursive(n, "(", 1, 0, results);
+        dfs(results, result, 0,0,n);
 
         return results;
     }
