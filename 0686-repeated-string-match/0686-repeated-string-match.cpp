f81436a33cf98a452a61598c60d6f29ca76f45bf
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2026-02-12 15:47:26 +0700

    Time: 2 ms (83.05%), Space: 10.7 MB (23.82%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -0,0 +1,77 @@
+class Solution {
+private:
+    vector<int> generateLPS(string pattern) {
+        vector<int> LPS(pattern.length(), 0);
+        int left = 0;
+        int right = 1;
+
+        while (right < pattern.length()) {
+            if (pattern[left] == pattern[right]) {
+                left++;
+                LPS[right] = left;
+                right++;
+            } else {
+                if (left == 0) {
+                    right++;
+                } else {
+                    left = LPS[left - 1];
+                }
+            }
+        }
+
+        return LPS;
+    }
+
+    bool check(string a, string b, vector<int> &LPS) {
+
+        int indexA = 0;
+        int indexB = 0;
+
+        while (indexA < a.length()) {
+            if (a[indexA] == b[indexB]) {
+                indexA++;
+                indexB++;
+            } else {
+                if (indexB == 0) {
+                    indexA++;
+                } else {
+                    indexB = LPS[indexB - 1];
+                }
+            }
+
+            if (indexB == b.length()) {
+                break;
+            }
+        }
+
+        if (indexB != b.length()) {
+            return false;
+        }
+
+        return true;
+    }
+public:
+    int repeatedStringMatch(string a, string b) {
+        vector<int> LPS = generateLPS(b);
+        int result = 1;
+
+        string temp = a;
+        while (a.length() < b.length()) {
+            result++;
+            a += temp;
+        }
+
+        if (check(a, b, LPS)) {
+            return result;
+        }
+
+        a += temp;
+        result++;
+
+        if (check(a, b, LPS)) {
+            return result;
+        }
+
+        return -1;
+    }
+};
