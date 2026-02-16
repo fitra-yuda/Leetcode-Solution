35b3a96436acc748d28687dc2ee9269e83aa19df
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2026-02-16 12:22:44 +0700

    Time: 15 ms (18.33%), Space: 10.9 MB (40.41%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,63 +1,60 @@
-/*
-
-How to solve the problem
-
-# Using sliding window
-1. Initialize left, right, bucket of characters, and result
-2. while right < s.length()
-   - get the ascii character, put that into the bucket
-   - get the maximum number and total in the bucket
-   - decrease total by maximum number
-   - while total > k
-     - should move the left pointer and remove the s[left] from the bucket, since this current window is not valid anymore
-     - get the maximum number and total in the bucket
-     - decrease total by maximum number
-   - get the result answer using this formula : result = max(result, right - left + 1)
-3. return result
-
-Time Complexity : O(N * M^2)
-N -> length of string s
-M -> total different characters in the bucket array -> worst case 26 characters
-
-Memory Complexity : O(M)
-M -> total different characters in the bucket array
-
-*/
-
-
 class Solution {
-public:
-    int characterReplacement(string s, int k) {
-        int result = 0;
-        vector<int> bucket(26, 0);
-        int left = 0;
-        int right = 0;
+private:
+    char getMaximumCharacter(unordered_map<char,int> &bucket){ 
+        char keyResult = ' ';
+        int maxValue = 0;
 
-        while (right < s.length()) {
-            int ascii = s[right] - 'A';
-            bucket[ascii]++;
+        for (auto [key, value] : bucket) {
+            if (key == ' ') {
+                keyResult = key;
+                maxValue = value;
+            } else if (maxValue < value) {
+                keyResult = key;
+                maxValue = value;
+            }
+        }
 
-            int maximum = 0;
-            int total = 0;
-            for (auto data : bucket) {
-                total += data;
-                maximum = max(maximum, data);
+        return keyResult;
+    }
+
+    int calculateKUsed(unordered_map<char,int> &bucket) {
+        char maximumCharacter = getMaximumCharacter(bucket);
+        int totalKUsed = 0;
+
+        for (auto [key, value] : bucket) {
+            if (key == maximumCharacter) {
+                continue;
             }
 
-            total -= maximum;
+            totalKUsed += value;
+        }
 
-            while (total > k) {
-                bucket[s[left] - 'A']--;
-                maximum = 0;
-                total = 0;
+        return totalKUsed;
+    }
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char,int> bucket;        
 
-                for (auto data : bucket) {
-                    total += data;
-                    maximum = max(maximum, data);
+        int left = 0;
+        int right = 0;
+        int result = 0;
+
+        while (right < s.length()) {
+            bucket[s[right]]++;
+
+            if (bucket.size() > 1) {
+                int kUsed = calculateKUsed(bucket);
+
+                while (kUsed > k) {
+                    bucket[s[left]]--;
+                    left++;
+
+                    if (bucket[s[left]] == 0) {
+                        bucket.erase(s[left]);
+                    }
+
+                    kUsed = calculateKUsed(bucket);
                 }
-                
-                total -= maximum;
-                left++;
             }
 
             result = max(result, (right - left) + 1);
