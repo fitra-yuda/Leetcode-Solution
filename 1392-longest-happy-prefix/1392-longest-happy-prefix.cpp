f13dd5b90e17a435821ba2bb45e87009e13a06dd
Fitrayuda <90248869+fitra-yuda@users.noreply.github.com>
2026-02-17 07:03:27 +0700

    Time: 8 ms (56.17%), Space: 24.5 MB (14.8%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    vector<int> generateLPS(string &pattern) {
+        vector<int> LPS(pattern.length(), 0);
+        int left = 0;
+        int right = 1;
+
+        while (right < pattern.length()) {
+            if (pattern[left] == pattern[right]) {
+                left++;
+                LPS[right] = left;
+                right++;
+            } else {
+                if (left == 0) {
+                    right++;
+                } else {
+                    left = LPS[left - 1];
+                }
+            }
+        }
+
+        return LPS;
+    }
+public:
+    string longestPrefix(string s) {
+        vector<int> LPS = generateLPS(s);
+        int patternLength = LPS[s.length() - 1];
+        string result = "";
+
+        for (int i = 0; i < patternLength; i++) {
+            result += s[i];
+        }
+
+        return result;
+    }
+};
